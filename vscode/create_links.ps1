afff829446f6b2008331c3744c7f5b5605efe2f4
siewertj <malgarith@outlook.com>
2019-09-04 18:49:33 -0700

    updated how I link the vscode settings

--- vscode/create_links.ps1
+++ vscode/create_links.ps1
@@ -1,8 +1,20 @@
 # Hardlink the files 
 $original_file = "${env:AppData}\Code\User\settings.json"
+$new_file = "${PSScriptRoot}\settings.json"
 if ( (Test-Path $original_file -PathType Leaf) ) {
-	Write-Host "'${original_file}' already exists. Moving to ${original_file}.bak."
-	Copy-Item -Path ${original_file} -Destination ${original_file}.bak
+	$backup_file = "${original_file}.bak"
+	Remove-Item -Path ${backup_file}
+	Write-Host "'${original_file}' already exists. Moving to ${backup_file}"
+	Move-Item -Path ${original_file} -Destination ${backup_file}
 }
-#cmd /c mklink /H 
-#$dest = "${PSScriptRoot}\settings.json"
+cmd /c mklink /H ${original_file} ${new_file}
+
+$original_file = "${env:AppData}\Code\User\keybindings.json"
+$new_file = "${PSScriptRoot}\keybindings.json"
+if ( (Test-Path $original_file -PathType Leaf) ) {
+	$backup_file = "${original_file}.bak"
+	Remove-Item -Path ${backup_file}
+	Write-Host "'${original_file}' already exists. Moving to ${backup_file}"
+	Move-Item -Path ${original_file} -Destination ${backup_file}
+}
+cmd /c mklink /H ${original_file} ${new_file}
